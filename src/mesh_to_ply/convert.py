10155cc635301e3026472eb6c8c66071b5a570e0
Louis Remes <louisremes@gmail.com>
2026-03-05 11:44:41 +0100

    Small corrections

--- src/mesh_to_ply/convert.py
+++ src/mesh_to_ply/convert.py
@@ -23,12 +23,12 @@ def convert(in_path, out_path: Path) -> None:
         out_points = np.empty((0,3), dtype = np.float64)
         out_tris = np.empty((0,3), dtype = np.int64)
         for region_id in set(mesh.cell_data_dict[key]['tetra']):
-            surface_points, surface_faces = surface_from_mesh(mesh.points, mesh.cells_dict['tetra'][mesh.cell_data_dict[key]['tetra'] == region_id,:])
+            surface_points, surface_faces = _surface_from_mesh(mesh.points, mesh.cells_dict['tetra'][mesh.cell_data_dict[key]['tetra'] == region_id,:])
 
             out_tris = np.vstack([out_tris, surface_faces + out_points.shape[0]])
             out_points = np.vstack([out_points, surface_points])
     else:
-        out_points, out_tris = surface_from_mesh(mesh.points, mesh.cells_dict['tetra'])
+        out_points, out_tris = _surface_from_mesh(mesh.points, mesh.cells_dict['tetra'])
 
     if out_tris.max() > np.iinfo(np.int32).max:
         raise OverflowError("PLY cannot store indices > int32")
@@ -42,21 +42,21 @@ def convert(in_path, out_path: Path) -> None:
 
     meshio.write(out_path, surface_mesh, file_format="ply")
 
-def surface_from_mesh(points, tets: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+def _surface_from_mesh(points, tets: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     tris = tets[:, [[0,2,1],[0,1,3],[1,2,3],[0,3,2]]].reshape(-1,3)
 
-    _, inverse, counts = np.unique(np.sort(tris), axis = 0, return_inverse = True, return_counts = True)
+    _, inverse, counts = np.unique(np.sort(tris, axis = 1), axis = 0, return_inverse = True, return_counts = True)
     is_boundary_face = counts[inverse] == 1
     boundary_tris = tris[is_boundary_face, :]
 
-    surface_point_id = np.unique(boundary_tris.flatten())
+    surface_point_id = np.unique(boundary_tris.ravel())
 
-    lut = np.full(surface_point_id.max()+1, 0, dtype=smallest_uint_dtype(surface_point_id.size-1))
+    lut = np.full(surface_point_id.max()+1, 0, dtype=_smallest_uint_dtype(surface_point_id.size-1))
     lut[surface_point_id] = np.arange(surface_point_id.size)
 
     return points[surface_point_id, :], lut[boundary_tris].astype(np.int64)
 
-def smallest_uint_dtype(max_value: int):
+def _smallest_uint_dtype(max_value: int):
     for dtype in (np.uint8, np.uint16, np.uint32, np.uint64):
         if max_value <= np.iinfo(dtype).max:
             return dtype
